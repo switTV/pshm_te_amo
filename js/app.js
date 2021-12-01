b0627179152827eec2d1d96e120c379c5d22883b
swittv <4galarzajuan2004@gmail.com>
2021-12-01 19:07:23 -0300

    Second commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var texto = document.getElementById("texto-datos")
+var boton_si = document.getElementById("boton-aceptar")
+
+boton_si = document.addEventListener("click",texto_aparecer)
+
+function texto_aparecer(){
+    texto.innerHTML="ahora tengo tu direccion IP puto pringao"
+}
