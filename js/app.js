1effa02cb444124f6eb4b9572ae839a94685d030
swittv <4galarzajuan2004@gmail.com>
2021-12-02 10:31:09 -0300

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var texto = document.getElementById("texto-datos")
 var boton_si = document.getElementById("boton-aceptar")
 
-boton_si = document.addEventListener("click",texto_aparecer)
+boton_si.addEventListener("click",texto_aparecer)
 
 function texto_aparecer(){
     texto.innerHTML="ahora tengo tu direccion IP puto pringao"
